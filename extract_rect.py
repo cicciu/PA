1df1ab6af3a1c59cc15202297349655b13c2a9de
ciccius <salvatore.cicciu@gmail.com>
2018-03-04 22:13:27 +0100

    rect detect and export rect

--- extract_rect.py
+++ extract_rect.py
@@ -6,51 +6,26 @@ from utils.imgProcessing import *
 
             
 imageFile = "Images/G00368188.jpg"
-#imageFile ="Images/G00009201.jpg"
+imageFile ="Images/G00009201.jpg"
 #imageFile = "Boiss/G00753100.tif"
 
 
 
 #Read image
 img = cv2.imread(imageFile)
-
-#filters
-imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #transgorm rgb to gray levelb
-imgBlur = cv2.medianBlur(imgray,5) #Remove salt and peper
-th, img_thresh = cv2.threshold(imgBlur, 195, 255, cv2.THRESH_BINARY) #Treeshold of image
+li = img.shape[0]
+col = img.shape[1]
 
 
-img_floodfill = img_thresh.copy()
- 
-# Mask used to flood filling.
-h, w = img_thresh.shape[:2]
-mask = np.zeros((h+2, w+2), np.uint8)
- 
-# Floodfill from point (0, 0)  all black in white start in 0,0
-cv2.floodFill(img_floodfill, mask, (0,0), 255);
- 
-# Invert floodfilled image (rect in white)
-im_floodfill_inv = cv2.bitwise_not(img_floodfill)
- 
-# Combine the two images to get the foreground
-im_out = img_thresh | im_floodfill_inv
+imgF = imgFilter(img)
 
-
-
-#show images
-titles = ['img_thresh', 'img_floodfill','im_floodfill_inv','im_out']
-#images = [img, img_thresh]
-
-images = [img_thresh, img_floodfill, im_floodfill_inv, im_out]
-
-for i in xrange(4):
-    plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
-    plt.title(titles[i])
-    plt.xticks([]),plt.yticks([])
-
-imageFilter = im_out#img_thresh
+plt.imshow(imgF)
 plt.show()
-plt.imshow(imageFilter)
 
 
+rectIm, rects = rectImg(imgF, li, col)
+
+plt.imshow(rectIm)
 plt.show()
+
+exportRects(img, rects, li, col, 5000)
--- utils/imgProcessing.py
+++ utils/imgProcessing.py
@@ -1,83 +1,101 @@
 #!/usr/bin/python2.7
 #-*- coding: utf-8 -*-
-from PIL import Image
-import sys
-from math import sqrt
+import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+def imgFilter(img):
+    imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #transgorm rgb to gray levelb
+    imgBlur = cv2.medianBlur(imgray,5) #Remove salt and peper
+    th, img_thresh = cv2.threshold(imgBlur, 195, 255, cv2.THRESH_BINARY) #Treeshold of image
+
+
+    img_floodfill = img_thresh.copy()
     
+    # Mask used to flood filling.
+    h, w = img_thresh.shape[:2]
+    mask = np.zeros((h+2, w+2), np.uint8)
+    
+    # Floodfill from point (0, 0)  all black in white start in 0,0
+    cv2.floodFill(img_floodfill, mask, (0,0), 255);
+    
+    # Invert floodfilled image (rect in white)
+    im_floodfill_inv = cv2.bitwise_not(img_floodfill)
+    
+    # Combine the two images to get the foreground
+    im_out = img_thresh | im_floodfill_inv
 
-def invert(image):
-    """this function make a negative image
+    return img_thresh
 
-    Prams :
-    - -- -
-    """
-    try:
-        img = Image.open(ImageFile)
-    except IOError:
-        print 'Erreur sur ouverture du fichier ' + ImageFile
-
-    newImage = Image.new(img.mode,img.size)
-
-    col,li = img.size
-
-    for i in range(li):
-        for j in range(col):
-            pixel = img.getpixel((j,i)) # recup du pixel
-            # on calcule le complement a MAX pour chaque composante - effet negatif
-            p = (255 - pixel[0], 255 - pixel[1], 255 - pixel[2])
-            # composition de la nouvelle image
-            newImage.putpixel((j,i), p)
-    return newImage
-
-def contourDetection(seuil, image):
-    newImage = image.new(img.mode,img.size)
-
-    col,li = img.size
-
-    for i in range(1,li-1):
-        for j in range(1,col-1):
-            p1 = img.getpixel((j-1,i))
-            p2 = img.getpixel((j,i-1))
-            p3 = img.getpixel((j+1,i))
-            p4 = img.getpixel((j,i+1))
-            n = norme(p1,p2,p3,p4)
-
-            if n < seuil:
-                p = (255,255,255)
-            else:
-                p = (0,0,0)
-            newImage.putpixel((j-1,i-1),p)
-    return newImage
-def norme(self,p1,p2,p3,p4):
-    n = sqrt((p1[0]-p3[0])*(p1[0]-p3[0]) + (p2[0]-p4[0])*(p2[0]-p4[0]))
-    return n
-"""
-def convolution2D(self,filt,TPix,x,y):
-    p0 = p1 = p2 = 0
-    for i in range(-1,1):
-        for j in range(-1,1):
-            p0 += filt[i+1][j+1]*TPix[y+i,x+j][0]
-            p1 += filt[i+1][j+1]*TPix[y+i,x+j][1]
-            p2 += filt[i+1][j+1]*TPix[y+i,x+j][2]
-            # normalisation des composantes
-            p0 = int(p0/9.0)
-            p1 = int(p1/9.0)
-            p2 = int(p2/9.0)
-    # retourne le pixel convolué
-    return (p0,p1,p2)
-
-def filter(self,filt):
-    newImage = Image.new(self.imageProcess.mode,self.imageProcess.size)
-    TabPixel = self.imageProcess.load()
-    for x in range(1,self.ligne-1):
-        for y in range(1,self.colonne-1):
-            p = self.convolution2D(filt,TabPixel,x,y)
-            newImage.putpixel((y,x),p)
-    self.imageProcess = newImage
-    return newImage
+def rectImg(img, li, col):
+        
+    # detect edges in the image
+    edged = cv2.Canny(img, 10, 250)
 
 
-"""
+    # construct and apply a closing kernel to 'close' gaps between 'white'
+    # pixels
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
+    closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
+
+
+    # find contours (i.e. the 'outlines') in the image and initialize the
+    # total number of books found
+    _ , contours, hierarchy = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    total = 0
+
+
+
+    rects = [cv2.boundingRect(cnt) for cnt in contours]
+    rects = sorted(rects,key=lambda  x:x[1],reverse=True)
+
+
+
+
+    #crate new image white
+    newImg = np.ones((li, col))
+
+    # loop over the contours
+    for c in contours:
+        # approximate the contour 
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+        
+        # if the approximated contour has four points, then assume that the
+        # contour is a book -- a book is a rectangle and thus has four vertices
+        if len(approx) == 4:
+            cv2.drawContours(newImg, [approx], -1, (0, 255, 0), 4)
+            total += 1
+    return newImg, rects
+
+def exportRects(img, rects, li, col, minArea):
+    i = -1
+    j = 1
+    y_old = li
+    x_old = col
+
+    for rect in rects:
+        x,y,w,h = rect
+        area = w*h
+        #test if area is not small
+        if area > minArea:
+            if (y_old - y) > 200:
+                    i += 1
+                    y_old = y
+
+            if abs(x_old - x) > 300:
+                x_old = x
+                x,y,w,h = rect
+
+                out = img[y+10:y+h-10,x+10:x+w-10]
+                j+=1
+                plt.imshow(out)
+                plt.show()
+                #export rects
+                #cv2.imwrite('cropped\\' + fileName[i] + '_' + str(j) + '.jpg', out)
+
+            
+
 
 
