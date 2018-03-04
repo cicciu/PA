70ee087b198cb1343988c0b48a11b05cabaa2569
ciccius <salvatore.cicciu@gmail.com>
2018-03-04 20:25:59 +0100

    filter rect with color

--- extract_rect.py
+++ extract_rect.py
@@ -6,53 +6,51 @@ from utils.imgProcessing import *
 
             
 imageFile = "Images/G00368188.jpg"
+#imageFile ="Images/G00009201.jpg"
 #imageFile = "Boiss/G00753100.tif"
 
 
 
 #Read image
 img = cv2.imread(imageFile)
-img = cv2.medianBlur(img,5)
 
-imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #transforme img in rgb
+#filters
+imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #transgorm rgb to gray levelb
+imgBlur = cv2.medianBlur(imgray,5) #Remove salt and peper
+th, img_thresh = cv2.threshold(imgBlur, 195, 255, cv2.THRESH_BINARY) #Treeshold of image
 
-ret, thresh = cv2.threshold(imgray, 195, 255, cv2.THRESH_BINARY) #treeshold
 
-titles = ['Original Image', 'Global Thresholding (v = 127)',]
-images = [img, thresh]
+img_floodfill = img_thresh.copy()
+ 
+# Mask used to flood filling.
+h, w = img_thresh.shape[:2]
+mask = np.zeros((h+2, w+2), np.uint8)
+ 
+# Floodfill from point (0, 0)  all black in white start in 0,0
+cv2.floodFill(img_floodfill, mask, (0,0), 255);
+ 
+# Invert floodfilled image (rect in white)
+im_floodfill_inv = cv2.bitwise_not(img_floodfill)
+ 
+# Combine the two images to get the foreground
+im_out = img_thresh | im_floodfill_inv
 
-for i in xrange(2):
+
+
+#show images
+titles = ['img_thresh', 'img_floodfill','im_floodfill_inv','im_out']
+#images = [img, img_thresh]
+
+images = [img_thresh, img_floodfill, im_floodfill_inv, im_out]
+
+for i in xrange(4):
     plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
     plt.title(titles[i])
     plt.xticks([]),plt.yticks([])
 
-#im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-#plt.imshow(img)
-
-"""
-#split in rgb
-blue,green,red = cv2.split(img)
-plt.imshow(red,cmap=plt.cm.gray)
-
-#Hough function to red (threeshold is threeshold of maxima in accumulator)
-lines = cv2.HoughLines(red,rho=1,theta=np.pi/180,threshold=130)
+imageFilter = im_out#img_thresh
+plt.show()
+plt.imshow(imageFilter)
 
 
-img2 = img.copy()
-for rho,theta in lines[0]:
-    a = math.cos(theta)
-    b = math.sin(theta)
-    x0 = a*rho
-    y0 = b*rho
-    x1 = int(x0 + 1000*(-b))
-    y1 = int(y0 + 1000*(a))
-    x2 = int(x0 - 1000*(-b))
-    y2 = int(y0 - 1000*(a))
-    cv2.line(img2,(x1,y1),(x2,y2),(0,0,255),2)
-plt.figure(figsize=(8,6))
-plt.imshow(img2)
-
-#image show in gray with red split
-#plt.imshow(red,cmap=plt.cm.gray)
-"""
 plt.show()
